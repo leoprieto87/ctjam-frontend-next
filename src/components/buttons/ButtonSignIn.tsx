c87dc78c75dca3119f41629a460e94eb66d3eec3
Leonardo Prieto <leonardo_santos_ext_8@carrefour.com>
2023-05-25 14:23:21 -0300

    Initial Commit

---
+++ src/components/buttons/ButtonSignIn.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export function ButtonSignIn() {
+
+    return (
+        <Link className="bg-sky-700 py-3 px-6 rounded-xl" href="/cadastro">Entrar</Link>
+    )
+  }
