b9132f72e4a74bffaab25bccb8d94e8c83cb3f85
Leonardo Prieto <leonardo_santos_ext_8@carrefour.com>
2023-06-16 17:12:19 -0300

    change htts to http

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       backgroundImage: {
         // 'logo': "url('http://www.ctjam.kinghost.net/images/logo-ctjam.png')",
         heroBanner:
-          "url('http://ctjam.nodejsng10f06.kinghost.net:21041/images/bgCtJamApp.jpg')",
+          "url('http://ctjam.nodejsng10f06.kinghost.net/images/bgCtJamApp.jpg')",
         'gradient-linear':
           'linear-gradient(180deg, rgba(218,218,218,1) 80%, rgba(255,255,255,1) 100%)',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
